851881b3812394ed4f98bf27972334b6eea2c4dd
Jimi Carlo Wills <jimi@cpan.org>
2014-01-06 20:41:37 +0000

    Import of JIMI/Tk-MIMEApp-0.02 from CPAN.
    gitpan-cpan-distribution: Tk-MIMEApp
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JIMI/Tk-MIMEApp-0.02.tar.gz
    gitpan-cpan-author:       JIMI
    gitpan-cpan-maturity:     released

--- lib/Tk/MIMEApp.pm
+++ lib/Tk/MIMEApp.pm
@@ -16,11 +16,11 @@ Tk::MIMEApp - The great new Tk::MIMEApp!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 our @Shelf = (); # we'll put our books here!
 
